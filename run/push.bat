2d3ef284fe21d6d5b8f216c1eb80d31f58438342
pfarrhoferphilip <p.pfarrhofer@students.htl-leonding.ac.at>
2026-01-27 08:58:45 +0100

    Worked on Docker

--- run/push.bat
+++ run/push.bat
@@ -1,5 +1,4 @@
 docker-compose build --no-cache
 
-docker push pfarrhoferphilip/wettbewerbs-keycloak
 docker push pfarrhoferphilip/wettbewerbs-frontend
 docker push pfarrhoferphilip/wettbewerbs-backend
